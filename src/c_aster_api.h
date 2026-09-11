4398e3c0742e83b72ecadb95a1b369225326aae2
rashidvali <rashid_vali@hotmail.com>
2026-09-11 15:39:39 -0700

    ++

--- src/c_aster_api.h
+++ src/c_aster_api.h
@@ -5,9 +5,11 @@ int *alnInt(int value);
 float *alnFloat(float value);
 char *alnChar(char value);
 char *alnStr(const char *value);
+
 int *alnIntArr(size_t count);
 float *alnFloatArr(size_t count);
 char *alnCharArr(size_t count);
+
 void dispose(void* ptr);
 
 #endif
